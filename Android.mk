309a4fca95f02ee3e297e65ea9cedd581982afa3
Amit Agrawal <quic_amag@quicinc.com>
2023-02-24 12:48:20 +0530

    wfd-opensource: guard compilation with Techpack variant Macro
    Change-Id: I1aabd1f7ac11bdc4b6e4756a4853b33613b0fe81

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(BUILD_VIDEO_TECHPACK_SOURCE), true)
 LOCAL_PATH := $(call my-dir)
 
 QMAA_DISABLES_WFD := false
@@ -23,3 +24,4 @@ include $(call all-makefiles-under, $(LOCAL_PATH))
 endif #QMAA_DISABLES_WFD
 endif #TARGET_HAS_LOW_RAM
 endif #WFD_DISABLE_PLATFORM_LIST
+endif # BUILD_VIDEO_TECHPACK_SOURCE
